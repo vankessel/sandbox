0fc8f6c612e0a3ada24b4383291443baa95c10ca
Kevin Van Kessel <10148659+vankessel@users.noreply.github.com>
2018-09-02 19:52:48 -0700

    pyc to gitignore

--- graph/main.py
+++ graph/main.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import dcoloring, render
 
-WIDTH = 24.0
-HEIGHT = 24.0
-POINTS_PER_DIM = 1000
-FRAMES = 12
+WIDTH = 16.0
+HEIGHT = 16.0
+POINTS_PER_DIM = 1024
+FRAMES = 120
+FPS = 24
 TEMP_DIR = 'temp'
 
 if not os.path.exists(TEMP_DIR):
@@ -24,10 +25,10 @@ z = x + 1j*y
 t = np.arange(0.0, 2*np.pi - 2*np.pi/FRAMES/2, 2*np.pi/FRAMES)
 weight1 = dcoloring.clover(   t          )
 bias1   = dcoloring.clover(   t, -np.pi/2)
-weight2 = dcoloring.clover( 4*t, -np.pi/2)
-bias2   = dcoloring.clover( 4*t          )
-weight3 = dcoloring.clover(16*t          )
-bias3   = dcoloring.clover(16*t, -np.pi/2)
+weight2 = dcoloring.clover( 2*t, -np.pi/2)
+bias2   = dcoloring.clover( 2*t          )
+weight3 = dcoloring.clover( 3*t          )
+bias3   = dcoloring.clover( 3*t, -np.pi/2)
 
 #This loop goes over each weight and bias and generates a plot for each
 #These plots are aggregated to make a video showcasing some of the function space
