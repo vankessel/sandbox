73244bd1b947d0b36b268cf1608bfb4c3dfad4f1
Kevin Van Kessel <10148659+vankessel@users.noreply.github.com>
2018-09-12 20:15:58 -0700

    Working commit

--- graph/interp.py
+++ graph/interp.py
@@ -4,18 +4,21 @@ import os
 from shutil import rmtree
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 import dcoloring, render
 
-WIDTH = 16.0
-HEIGHT = 16.0
-POINTS_PER_DIM = 512
-FRAMES = 60
-FPS = 12
+FILE_NAME = 'exp(z)'
+WIDTH = 16
+HEIGHT = 16
+POINTS_PER_DIM = 2048
+FRAMES = 240
+FPS = 60
 BACK_FORTH = True
 TEMP_DIR = 'temp'
+OUT_DIR = 'out'
 
-if not os.path.exists(TEMP_DIR):
-    os.makedirs(TEMP_DIR)
+if not os.path.exists(OUT_DIR):
+    os.makedirs(OUT_DIR)
 
 x, y = np.ogrid[
     -WIDTH/2:WIDTH/2:POINTS_PER_DIM*1j,
@@ -23,33 +26,79 @@ x, y = np.ogrid[
 ]
 z = x + 1j*y
 
+# 0 to 1 inclusive
 lerp = np.arange(0.0, 1.0 + 1.0/FRAMES/2, 1.0/FRAMES)
 cerp = dcoloring.cos_interpolation(lerp)
 
-#Animate transistion between two complex functions
+cfunctions = [
+    # ('exp(z)', np.exp(z)),
+    # ('ln(z)', np.log(z)),
+    # ('z^3', z*z*z),
+    # ('z^2', z*z),
+    # ('z^0.5', np.sqrt(z)),
+    # ('z^-1', 1/z),
+    # ('z^-0.5', 1/np.sqrt(z)),
+    # ('z^-2', 1/(z*z)),
+    # ('sin(z)', np.sin(z)),
+    # ('sinh(z)', np.sinh(z)),
+    # ('asin(z)', np.arcsin(z)),
+    # ('tan(z)', np.tan(z)),
+    # ('Zoomed sin(z^-1)', np.sin(1/z)),
+    # ('Zoomed tan(z^-1)', np.tan(1/z)),
+    # ('z^i', np.power(z, 1j)),
+    # ('z^-i', np.power(z, -1j)),
+    ('1/(1+e^-x)', 1/(1 + np.exp(-z))),
+    ('e^(-e^-x)', np.exp(-np.exp(-z)))
+]
 
-file_names = []
-fig = plt.figure()
-ax = fig.add_subplot(111)
+for func_name, cfunction in cfunctions:
+    print('Processing {}'.format(func_name))
 
-interp = cerp
-for idx in range(0, len(interp)):
-    w = z * (1.0 - interp[idx]) + 1/(z*z*z) * (interp[idx])
+    # Perpare plots and function
 
-    img = dcoloring.colorize(w, grid=False)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-    ax.clear()
-    ax.imshow(img, extent=(-WIDTH/2,WIDTH/2,-HEIGHT/2,HEIGHT/2))
-    ax.set_xlim(-WIDTH/2, WIDTH/2)
-    ax.set_ylim(-HEIGHT/2, HEIGHT/2)
-    ax.set(xlabel='{}'.format(idx))
 
-    print('Rendering frame {0:{2}}/{1:{2}}'.format(idx, FRAMES, int(np.log10(FRAMES)+1)))
-    fig.savefig('{}/frame.{}.png'.format(TEMP_DIR, idx))
-    file_names.append('{}/frame.{}.png'.format(TEMP_DIR, idx))
+    # Set interpolation and animate transition between two complex functions
 
-if BACK_FORTH:
-    file_names = file_names + list(reversed(file_names[1:-1]))
+    file_names = []
+    path = OUT_DIR + '/' + func_name
+    if not os.path.exists(TEMP_DIR):
+        os.makedirs(TEMP_DIR)
 
-render.create_webm('graph', file_names)
-rmtree(TEMP_DIR)
+    interp = cerp
+    for idx in range(0, len(interp)):
+        w = z * (1.0 - interp[idx]) + cfunction * (interp[idx])
+
+        cfunc_plot = dcoloring.colorize(w, grid=False)
+
+        ax.clear()
+        ax.imshow(cfunc_plot, extent=(-WIDTH/2, WIDTH/2, -HEIGHT/2, HEIGHT/2))
+        ax.set(xlabel='{:3.0f}%'.format(interp[idx] * 100), title=func_name)
+
+        # Save frame
+        print('Rendering frame {0:{2}}/{1:{2}}'.format(idx, FRAMES, int(np.log10(FRAMES) + 1)))
+        temp_path = '{}/frame.{}.png'.format(TEMP_DIR, idx)
+        fig.savefig(temp_path, dpi=600, transparent=True)
+        file_names.append(temp_path)
+
+        # Resize for aliasing
+        img = cv2.imread(temp_path)
+        img = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(temp_path, img)
+
+        # Save image of complete function
+        if idx == FRAMES - 1:
+            img_path = '{}.png'.format(path)
+            fig.savefig(img_path, dpi=1600, transparent=True)
+            img = cv2.imread(img_path)
+            img = cv2.resize(img, (int(img.shape[1]/4), int(img.shape[0]/4)), interpolation=cv2.INTER_AREA)
+            cv2.imwrite('{}.png'.format(path), img)
+
+    if BACK_FORTH:
+        file_names = file_names + list(reversed(file_names[1:-1]))
+
+    render.create_webm(path, file_names, fps=FPS, bitrate='8162k')
+    rmtree(TEMP_DIR)
+    plt.close('all')
